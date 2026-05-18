a529bbcec1859dddf0b0d422e13c5674b394807f
shivji-dev <shivjijha.dev@gmail.com>
2026-05-18 18:32:37 +0530

    fixed duplicates cors

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,6 @@ http.createServer(app);
 
 // Socket.io
 const io = new Server(server, {
-  cors: {
     cors: {
   origin: [
   "http://localhost:5173",
@@ -32,7 +31,7 @@ const io = new Server(server, {
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 
-  }
+}
 });
 
 global.io = io;
