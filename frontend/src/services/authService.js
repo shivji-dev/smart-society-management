57848a718f76e43db71df5c16bfd757f9678f2db
shivji-dev <shivjijha.dev@gmail.com>
2026-05-18 21:24:32 +0530

    fixed auth service and token save

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,31 +1,30 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL+"/auth",
+  baseURL: import.meta.env.VITE_API_URL + "/auth",
 });
 
+export const loginUser = async (userData) => {
 
+  const response = await API.post(
+    "/login",
+    userData
+  );
 
-export const loginUser =
-async (userData) => {
-
-  const response =
-    await axios.post(
-      `${API}/login`,
-      userData
-    );
+  localStorage.setItem(
+    "token",
+    response.data.token
+  );
 
   return response.data;
 };
 
-export const registerUser =
-async (userData) => {
+export const registerUser = async (userData) => {
 
-  const response =
-    await axios.post(
-      `${API}/register`,
-      userData
-    );
+  const response = await API.post(
+    "/register",
+    userData
+  );
 
   return response.data;
 };
