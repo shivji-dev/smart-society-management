b0a5e149f9c04336413064ae7b0c396c55a73b5a
shivji-dev <shivjijha.dev@gmail.com>
2026-05-18 18:10:04 +0530

    fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,8 +12,6 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-
     'https://smart-society-management-173f.vercel.app'
   ],
   credentials: true
